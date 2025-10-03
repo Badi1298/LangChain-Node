63bf53a7b25baf3a4a3ee85d1b58403308623133
Badi1298 <serbandavid83@yahoo.com>
2025-10-03 15:37:41 +0300

    feat: add prefill panel fields configuration for product details processing

--- src/app.js
+++ src/app.js
@@ -2,9 +2,6 @@ const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const pdfRoutes = require("./routes/parsePdfRoutes");
-const stockSuggestionsRoutes = require("./routes/stockSuggestionsRoutes");
-
 // Create an Express app
 const app = express();
 const PORT = 8002;
@@ -16,11 +13,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors());
-
-// Define an endpoint to handle file upload and parse
-app.use("/", pdfRoutes);
-
-app.use("/ai", stockSuggestionsRoutes);
 
 // Start the server
 app.listen(PORT, () => {
