f0cbdf43af023ae3bad71017c1b7a891579ef6d8
Badi1298 <serbandavid83@yahoo.com>
2025-10-27 10:46:22 +0200

    feat: add new constant types for barrier, autocall triggers, memory, size, dividend, FX risk, and offering

--- src/config/constants.js
+++ src/config/constants.js
@@ -7,6 +7,13 @@ const ProductTypesId = Object.freeze({
 	REVERSE_CONVERTIBLE: 9,
 });
 
+const BarrierTypes = Object.freeze({
+	LOW_STRIKE_LEVEL: 1,
+	EUROPEAN_CAPITAL_BARRIER: 2,
+	AMERICAN_CAPITAL_BARRIER: 3,
+	DAILY_CLOSE_BARRIER: 4,
+});
+
 const Frequencies = Object.freeze({
 	NO_COUPON: 0,
 	MONTHLY: 1,
@@ -17,9 +24,52 @@ const Frequencies = Object.freeze({
 	IN_FINE: 6,
 });
 
+const AutocallTriggerTypes = Object.freeze({
+	FIX: 1,
+	STEPDOWN: 2,
+});
+
+const MemoryTypes = Object.freeze({
+	YES: 1,
+	NO: 2,
+});
+
+const SizeTypes = Object.freeze({
+	NOTIONAL: 1,
+	UNITS: 2,
+});
+
 const RedemptionTypes = Object.freeze({
 	CASH: 1,
 	PHYSICAL: 2,
 });
 
-module.exports = { IssuersId, ProductTypesId, Frequencies, RedemptionTypes };
+const DividendTypes = Object.freeze({
+	DISCOUNTED: 1,
+	REINVESTED: 2,
+	IN_CASH_POCKET: 3,
+});
+
+const FxRiskTypes = Object.freeze({
+	HEDGED: 1,
+	NON_HEDGED: 2,
+});
+
+const SesameOfferingTypes = Object.freeze({
+	YES: 1,
+	NO: 2,
+});
+
+module.exports = {
+	IssuersId,
+	ProductTypesId,
+	BarrierTypes,
+	Frequencies,
+	AutocallTriggerTypes,
+	MemoryTypes,
+	SizeTypes,
+	RedemptionTypes,
+	DividendTypes,
+	FxRiskTypes,
+	SesameOfferingTypes,
+};
